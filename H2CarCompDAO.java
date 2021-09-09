c90ca09bc494df9ede84f655803b8e102993c797
mikestagney <mikestagney@yahoo.com>
2021-09-09 04:16:58 -0400

    saving progress

--- H2CarCompDAO.java
+++ H2CarCompDAO.java
@@ -2,6 +2,7 @@ package carsharing;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +13,8 @@ public class H2CarCompDAO implements CarDAO {
     static String DB_URL = "jdbc:h2:./src/carsharing/db/";
     Connection conn = null;
     Statement stmt = null;
-    List companies;
+    PreparedStatement prepStmt = null;
+    List<CarCompany> companies;
 
     public H2CarCompDAO(String dataSource) {
         try {
@@ -21,7 +23,7 @@ public class H2CarCompDAO implements CarDAO {
             conn.setAutoCommit(true);
             stmt = conn.createStatement();
 
-            String sql = "CREATE TABLE COMPANY (" +
+            String sql = "CREATE TABLE company (" +
                     "ID INT PRIMARY KEY AUTO_INCREMENT, " +
                     "NAME VARCHAR(30)) UNIQUE NOT NULL";
             stmt.execute(sql);
@@ -33,10 +35,36 @@ public class H2CarCompDAO implements CarDAO {
 
     @Override
     public void addCompany(String name) {
-
+        String insert = "INSERT INTO company (name) VALUES (?)";
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL);
+            conn.setAutoCommit(true);
+            prepStmt = conn.prepareStatement(insert);
+            prepStmt.setString(1, name);
+            prepStmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     @Override
     public List<CarCompany> getAllCompanies() {
+        companies = new ArrayList<>();
+        String select = "SELECT id, name FROM company ORDER BY id";
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL);
+            conn.setAutoCommit(true);
+            // while
+            prepStmt = conn.prepareStatement(select);
+            //prepStmt.setString(1, name);
+            prepStmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
 
         return new ArrayList<>();
     }
--- Main.java
+++ Main.java
@@ -1,42 +1,78 @@
 package carsharing;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     static final String JDBC_DRIVER = "org.h2.Driver";
     static String DB_URL = "jdbc:h2:./src/carsharing/db/";   //  "jdbc:h2:file:../task/src/carsharing/db/"
-    static final String USER = "JB";
-    static final String PASS ="Java";
-
+    static CarDAO dao;
+    static Scanner input;
 
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
-
+        input = new Scanner(System.in);
         String filename = "test";
         for (int i = 0; i < args.length; i++) {
             if ("-databaseFileName".equals(args[i]) && i + 1 < args.length) {
                 filename = args[i + 1];
             }
         }
-        DB_URL += filename;
+        dao = new H2CarCompDAO(filename);
+        mainMenu();
+    }
+    public static void mainMenu() {
+        while (true) {
+            System.out.println("1. Log in as a manager");
+            System.out.println("0. Exit");
+            String choice = input.nextLine();
 
-        try {
-            Class.forName(JDBC_DRIVER);
-
-            conn = DriverManager.getConnection(DB_URL);
-            conn.setAutoCommit(true);
-
-            stmt = conn.createStatement();
-
-            String sql = "CREATE TABLE COMPANY (" +
-                    "ID INT, " +
-                    "NAME VARCHAR(30))";
-            stmt.execute(sql);
-        } catch (Exception e) {
-            e.printStackTrace();
+            switch (choice.charAt(0)) {
+                case ('1'):
+                    managerMenu();
+                    break;
+                case ('0'):
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Not a valid option");
+                    break;
+            }
         }
     }
+    public static void managerMenu() {
+        while (true) {
+            System.out.println("1. Company list");
+            System.out.println("2. Create a company");
+            System.out.println("0. Back");
+            String choice = input.nextLine();
+            switch (choice.charAt(0)) {
+                case ('1'):
+                    listAllCompanies();
+                    break;
+                case ('2'):
+                    System.out.println("Enter the company name:");
+                    choice = input.nextLine();
+                    dao.addCompany(choice);
+                    break;
+                case ('0'):
+                    return;
+                default:
+                    System.out.println("Not a valid option");
+                    break;
+            }
+        }
+    }
+    public static void listAllCompanies() {
+        List<CarCompany> companies = dao.getAllCompanies();
+
+        if (companies.isEmpty()) {
+            System.out.println("The company list is empty!");
+        } else {
+            for (int i = 0; i < companies.size(); i++) {
+                System.out.printf("%d. %s \n", i + 1, companies.get(i).getName());
+            }
+        }
+    }
+
+
 }
